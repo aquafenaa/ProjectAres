8777690ff770f16e0d9244fe8b0d3176f1a2d801
Athena Zimmerman <aquafenaa1@gmail.com>
2025-05-24 17:44:06 -0400

    New recipes & Some filler item textures

--- run/kubejs/server_scripts/recipes.js
+++ run/kubejs/server_scripts/recipes.js
@@ -1,25 +1,25 @@
-// priority: 1
-// requires: create
-// requires: project_ares
-// requires: destroy
+// // priority: 1
+// // requires: create
+// // requires: project_ares
+// // requires: destroy
 
-// Visit the wiki for more info - https://kubejs.com/
+// // Visit the wiki for more info - https://kubejs.com/
 
 ServerEvents.recipes(recipeRegister => {
   recipeRegister.remove({ id: 'create_new_age:shaped/generator_coil' });
 
-  // recipeRegister.recipes.create.
+//   // recipeRegister.recipes.create.
 
-  recipeRegister.recipes.create.mechanical_crafting('create_new_age:generator_coil', [
-    ' CCC ',
-    'CPHPC',
-    'CHSHC',
-    'CPHPC',
-    ' CCC '
-  ], {
-    C: 'project_ares:finished_stator',
-    P: 'destroy:stainless_steel_sheet',
-    H: 'create:shaft',
-    S: 'project_ares:finished_rotor'
-  })
+//   recipeRegister.recipes.create.mechanical_crafting('create_new_age:generator_coil', [
+//     ' CCC ',
+//     'CPHPC',
+//     'CHSHC',
+//     'CPHPC',
+//     ' CCC '
+//   ], {
+//     C: 'project_ares:finished_stator',
+//     P: 'destroy:stainless_steel_sheet',
+//     H: 'create:shaft',
+//     S: 'project_ares:finished_rotor'
+//   })
 });
